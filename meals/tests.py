d9fb84fac62c5ae7b38ffa22bbce1b2eceb05395
Goluxas <douglasstorz@gmail.com>
2014-03-15 00:04:09 -0400

    Added first unit test and mapped root url to dummy view

--- meals/tests.py
+++ meals/tests.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from meals.views import home_page
 
 class SmokeTest(TestCase):
 	
-	def test_bad_maths(self):
-		self.assertEqual(1,2)
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
