e962ec035b7e166c88a3cfbeddde2dfdc36c8e50
Goluxas <douglasstorz@gmail.com>
2014-03-16 19:39:34 -0400

    Extended FT to test that users meals remain separate and can be retrieved at a specific URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -18,45 +18,68 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_adding_entrees_and_view_them_later(self):
-		# User wants to add a new entree that they learned how to cook.
-		# They fire up their browser and navigate to the MealPlan site.
+		# Mallory wants to add a new entree that she learned how to cook.
+		# She fires up her browser and navigates to the MealPlan site.
 		self.browser.get(self.live_server_url)
 
-		# User notices page title and header mention MealPlan
+		# Mallory notices page title and header mention MealPlan
 		self.assertIn('MealPlan', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('MealPlan', header_text)
 
-		# User is presented with the option to add an entree right away
+		# Mallory is presented with the option to add an entree right away
 		inputbox = self.browser.find_element_by_id('id_new_entree')
 		self.assertEqual(
 				inputbox.get_attribute('placeholder'),
 				'Enter an entree'
 		)
 
-		# User types "Cheese Omelette" into the text box
+		# Mallory types "Cheese Omelette" into the text box
 		inputbox.send_keys('Cheese Omelette')
 
-		# When user hits enter, page updates and now lists
+		# Mallory hits enter, page updates and now lists
 		# "Cheese Omelette (entree)"
 		inputbox.send_keys(Keys.ENTER)
+		mallory_meals_url = self.browser.current_url
+		self.assertRegex(mallory_meals_url, '/meals/.+')
+		self.check_for_row_in_entree_table('Cheese Omelette (entree)')
 
-		self.check_for_row_in_entree_table('Cheese Omelette')
-
-		# Text box remains. User also types "Chicken Fajitas" and confirms.
+		# Text box remains. Mallory also types "Chicken Fajitas" and confirms.
 		inputbox = self.browser.find_element_by_id('id_new_entree')
 		inputbox.send_keys('Chicken Fajitas')
 		inputbox.send_keys(Keys.ENTER)
 
 		# Page updates, now shows both Cheese Omelette and
 		# "Chicken Fajitas (entree)"
-		self.check_for_row_in_entree_table('Cheese Omelette')
-		self.check_for_row_in_entree_table('Chicken Fajitas')
+		self.check_for_row_in_entree_table('Cheese Omelette (entree)')
+		self.check_for_row_in_entree_table('Chicken Fajitas (entree)')
 
-		# User wonders if they can get back to this later. Some text on the
-		# page mentions that a unique URL has been generated to allow that.
+		# A second user, Scott, comes to the site
 
-		# User visits that URL, confirms it indeed stored the food items.
+		## New browser session to ensure that nothing from Mallory gets through
+		self.browser.quit()
+		self.browser = webdriver.Chrome()
 
-		self.fail('Finish the test!')
+		# Scott visits the home page. There's no sign of Mallory's meals.
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body')
+		self.assertNotIn('Cheese Omelette', page_text)
+		self.assertNotIn('Chicken Fajitas', page_text)
+
+		# Scott enters his first entree, starting a new meal collection
+		inputbox = self.browser.get_element_by_id('id_new_entree')
+		inputbox.send_keys('PB&J')
+		inputbox.send_keys(Keys.ENTER)
+
+		# Scott gets his own unique URL
+		scott_meals_url = self.broswer.current_url
+		self.assertRegex(scott_meals_url, '/meals/.+')
+		self.assertNotEqual(scott_meals_url, mallory_meals_url)
+
+		# Again there's no sign of Mallory's meals
+		page_text = self.browser.find_element_by_tag_name('body')
+		self.assertNotIn('Cheese Omelette', page_text)
+		self.assertIn('PB&J', page_text)
+
 		# Satisfied, user closes browser.
+		self.fail('Finish the test!')
