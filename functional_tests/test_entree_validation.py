d28feffdef98a3e8a2b68893677aca709bc3cca9
Goluxas <douglasstorz@gmail.com>
2014-03-18 14:16:48 -0400

    Added FT for new entree item validation

--- functional_tests/test_entree_validation.py
+++ functional_tests/test_entree_validation.py
@@ -11,15 +11,33 @@ class EntreeValidationTest(FunctionalTest):
 	def test_cannot_add_empty_entrees(self):
 		# Mallory goes to the home page and accidentally tries to submit
 		# an empty entree name. She hits enter on the empty input box
+		self.browser.get(self.server_url)
+		inputbox = self.browser.find_element_by_id('id_new_entree')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The home page refreshes and there is an error message saying
 		# that entree names cannot be blank
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, 'Entree name must not be blank')
 
 		# She tries again with some text for the item which now works
+		inputbox = self.browser.find_element_by_id('id_new_entree')
+		inputbox.send_keys('some text\n')
+
+		self.check_for_row_in_entree_table('some text')
 
 		# Perversely, she now decides to submit a second blank entree name
+		inputbox = self.browser.find_element_by_id('id_new_entree')
+		inputbox.send_keys(Keys.ENTER)
 
 		# She receives a similar warning on the arsenal page
+		self.check_for_row_in_entree_table('some text (entree)')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, 'Entree name must not be blank')
 
 		# And she can correct it by filling some text in
-		self.fail('write me!')
+		inputbox = self.browser.find_element_by_id('id_new_entree')
+		inputbox.send_keys('some different text\n')
+
+		self.check_for_row_in_entree_table('some text')
+		self.check_for_row_in_entree_table('some different text')
